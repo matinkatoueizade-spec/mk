adefefdfceffee45514524009835c9e9dc474a6a
متین <matin.katoueizade@gmail.com>
2026-06-28 14:59:01 +0330

    Update server.js

--- server.js
+++ server.js
@@ -5,24 +5,79 @@ const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", (socket) => {
-  console.log("User connected");
+const users = {};
 
-  socket.on("chat message", (data) => {
-    io.emit("chat message", data);
+io.on("connection", (socket) => {
+
+  console.log("Connected:", socket.id);
+
+  socket.on("join", (username) => {
+
+    users[socket.id] = {
+      id: socket.id,
+      username
+    };
+
+    io.emit("system", {
+      type: "join",
+      text: `${username} وارد گروه شد 🚀`
+    });
+
+    io.emit("users", Object.values(users));
+
+  });
+
+  socket.on("group-message", (data) => {
+
+    io.emit("group-message", {
+      user: data.user,
+      text: data.text,
+      time: Date.now()
+    });
+
+  });
+
+  socket.on("private-message", (data) => {
+
+    io.to(data.to).emit("private-message", {
+      from: users[socket.id]?.username || "Unknown",
+      text: data.text,
+      time: Date.now()
+    });
+
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+
+    if (users[socket.id]) {
+
+      io.emit("system", {
+        type: "leave",
+        text: `${users[socket.id].username} خارج شد 👋`
+      });
+
+      delete users[socket.id];
+
+      io.emit("users", Object.values(users));
+    }
+
+    console.log("Disconnected:", socket.id);
+
   });
+
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server Started On Port", PORT);
 });
